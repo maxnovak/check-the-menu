d8fceff5afaac1ed4adce0bdd3a7fa0a894ae810
Maxwell Novak <novak.max@gmail.com>
2023-09-04 12:51:02 -0400

    Create loader and load in assets for game

---
+++ src/assets.ts
@@ -0,0 +1,118 @@
+import type { ResolverManifest } from "pixi.js";
+
+export const manifest:ResolverManifest = {
+    bundles: [
+        {
+            name : "bundleName",
+            assets:
+            {
+                "characterSprites.png" : "./characterSprites.png",
+            }
+        }
+    ]
+};
+
+export const CharacterSprites = {
+    frames: {
+        idle1: {
+            frame: { x: 0, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle2: {
+            frame: { x: 48, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle3: {
+            frame: { x: 96, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle4: {
+            frame: { x: 144, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle5: {
+            frame: { x: 192, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle6: {
+            frame: { x: 240, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle7: {
+            frame: { x: 288, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle8: {
+            frame: { x: 336, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle9: {
+            frame: { x: 384, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        idle10: {
+            frame: { x: 432, y: 0, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run1: {
+            frame: { x: 0, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run2: {
+            frame: { x: 48, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run3: {
+            frame: { x: 96, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run4: {
+            frame: { x: 144, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run5: {
+            frame: { x: 192, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run6: {
+            frame: { x: 240, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run7: {
+            frame: { x: 288, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        },
+        run8: {
+            frame: { x: 336, y: 48, w: 48, h: 48 },
+            sourceSize: { w: 48, h: 48 },
+            spriteSourceSize: { x: 0, y: 0, w: 48, h: 48 }
+        }
+    },
+    meta: {
+		image: './characterSprites.png',
+		format: 'RGBA8888',
+		size: { w: 480, h: 48 },
+		scale: '1'
+	},
+	animations: {
+		playerIdle: ['idle1', 'idle2', 'idle3', 'idle4', 'idle5', 'idle6', 'idle7', 'idle8', 'idle9', 'idle10'],
+		playerRun: ['run1', 'run2', 'run3', 'run4', 'run5', 'run6', 'run7', 'run8']
+	}
+}
---
+++ src/scenes/Loader.ts
@@ -0,0 +1,59 @@
+import { Container, Graphics, Assets, Spritesheet, BaseTexture } from "pixi.js";
+import { CharacterSprites, manifest } from "../assets";
+import { Manager } from "../Manager";
+import { GameScene } from "./Game";
+import { IScene } from "./types";
+
+export class LoaderScene extends Container implements IScene {
+    private loaderBar: Container;
+    private loaderBarBoder: Graphics;
+    private loaderBarFill: Graphics;
+
+    constructor() {
+        super();
+
+        const loaderBarWidth = Manager.width * 0.8;
+
+        this.loaderBarFill = new Graphics();
+        this.loaderBarFill.beginFill(0x008800, 1)
+        this.loaderBarFill.drawRect(0, 0, loaderBarWidth, 50);
+        this.loaderBarFill.endFill();
+        this.loaderBarFill.scale.x = 0;
+
+        this.loaderBarBoder = new Graphics();
+        this.loaderBarBoder.lineStyle(10, 0x0, 1);
+        this.loaderBarBoder.drawRect(0, 0, loaderBarWidth, 50);
+
+        this.loaderBar = new Container();
+        this.loaderBar.addChild(this.loaderBarFill);
+        this.loaderBar.addChild(this.loaderBarBoder);
+        this.loaderBar.position.x = (Manager.width - this.loaderBar.width) / 2;
+        this.loaderBar.position.y = (Manager.height - this.loaderBar.height) / 2;
+        this.addChild(this.loaderBar);
+
+        this.initializeLoader().then(() => {
+            this.gameLoaded();
+        })
+    }
+
+    private async initializeLoader(): Promise<void> {
+        await Assets.init({ manifest: manifest });
+
+        const bundleIds =  manifest.bundles.map(bundle => bundle.name);
+        const playerSprites = new Spritesheet(BaseTexture.from(CharacterSprites.meta.image), CharacterSprites)
+        await playerSprites.parse();
+        Manager.playerSprites = playerSprites;
+
+        await Assets.loadBundle(bundleIds, this.downloadProgress.bind(this));
+    }
+
+    private downloadProgress(progressRatio: number): void {
+        this.loaderBarFill.scale.x = progressRatio;
+    }
+
+    private gameLoaded(): void {
+        Manager.changeScene(new GameScene());
+    }
+
+    public update(): void {}
+}
