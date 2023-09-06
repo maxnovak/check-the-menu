de919bacbd0bb4c274f15a3b9ff2adb8d07da82c
Maxwell Novak <novak.max@gmail.com>
2023-09-06 17:09:10 -0400

    Use pixijs UI List to make menu elements line up nicer

--- src/scenes/Menu.ts
+++ src/scenes/Menu.ts
@@ -1,6 +1,7 @@
 import { Container, Graphics, Text } from "pixi.js";
 import { IScene } from "./types";
 import { Manager } from "../Manager";
+import { List } from "@pixi/ui";
 
 export class MenuScene extends Container implements IScene {
     constructor() {
@@ -8,7 +9,7 @@ export class MenuScene extends Container implements IScene {
         const menuContainer = new Container();
         menuContainer.x = Manager.width / 2;
         menuContainer.y = Manager.height / 2;
-        const menuBox = new Graphics().beginFill('red').drawRect(-200, -50, 400, 100);
+        const menuBox = new Graphics().beginFill('red').drawRoundedRect(-200, -50, 400, 200, 20);
         const menuText = new Text("Main Menu", {
             fontSize: 36,
             fontWeight: "bold",
@@ -16,9 +17,24 @@ export class MenuScene extends Container implements IScene {
         });
         menuText.anchor.x = 0.5;
         menuText.anchor.y = 0.5;
+        const levelOneSelect = new Text("Level One", {
+            fontSize: 36,
+            fontWeight: "bold",
+            fontFamily: "Georgia, serif",
+        });
+        levelOneSelect.anchor.x = 0.5;
+        levelOneSelect.anchor.y = 0.5;
+
         menuContainer.addChild(menuBox);
-        menuContainer.addChild(menuText);
-        
+        const list = new List({
+            elementsMargin: 10,
+            vertPadding: 10,
+            type: "vertical",
+        });
+        list.addChild(menuText);
+        list.addChild(levelOneSelect);
+        menuContainer.addChild(list);
+
         this.addChild(menuContainer);
 
         document.addEventListener("keydown", this.onKeyDown.bind(this));
